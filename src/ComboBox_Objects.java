abfbc732e528503317ed613469373a339274a9ac
Areezy <46043090+Areezy@users.noreply.github.com>
2020-06-25 16:19:21 +0300

    Add files via upload

---
+++ src/ComboBox_Objects.java
@@ -0,0 +1,31 @@
+
+public class ComboBox_Objects {
+	
+//	combobox items class
+	
+	private String key;
+    private String value;
+
+    public ComboBox_Objects(String key)
+    {
+        this.key = key;
+       
+    }
+
+    @Override
+    public String toString()
+    {
+        return key; 
+    }
+
+    public String getKey()
+    {
+        return key;
+    }
+
+//    public String getValue()
+//    {
+//        return value;
+//    }
+
+}
